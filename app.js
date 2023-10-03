472a225c1880d7a2d264391763fb80fae14c56ea
muhammadimam155 <imamaliatari@gmail.com>
2023-10-04 00:27:07 +0500

    var for num

---
+++ app.js
@@ -0,0 +1,25 @@
+var age=18
+alert ("i am "+age+" year old")
+
+
+
+var visit=0
+visit++
+alert("you have visit this site "+visit+" times")
+
+
+
+
+var birthYear=2005
+document.write("my birth year is "+birthYear+" <br> data type of my declared varaible is number <br>")
+
+
+
+var visitorName = prompt ("visitor ; name" , " imam" )
+document.write("visitor name is "+visitorName+" <br> ")
+var productTitle = prompt ("product title ")
+document.write("visitor product is "+productTitle+" <br>")
+var order = prompt ("how many product you order")
+document.write("visitor has "+order+" order <br>")
+
+
